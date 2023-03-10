335d84054b39c341cdd278b322714c048f9d1dd5
Fernando <fh1273509@gmail.com>
2023-03-09 22:03:43 -0300

    configuracion ruta y ejs

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ app.listen('8000', (req, res) =>{
     console.log('aplicacion iniciada en localhost:8000');//puerto escuchando en el puerto 8000
 });
 
+app.set('views', './vistas'); //de aca recoge las vistas
+app.set('view engine', 'ejs'); //le digo que utilize ejs
+
+app.use(express.static('./estilos'));//configura los archivos estaticos
+
+
 //primera ruta:
 
 app.get('/', (req, res) =>{
     res.send('Funciona todo');
+    res.render('index');
 });
